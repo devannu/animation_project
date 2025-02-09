1b8058ea7b64993747d7068dfd3144f86fbb01bd
Annu Kumari <160406486+devannu@users.noreply.github.com>
2025-02-09 09:07:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+gsap.to("a",{
+    duration:0.5,
+    delay:0.2,
+    y:10,
+    stagger:0.3,
+    yoyo:true,
+   
+
+    
+    
+})
+
+gsap.to(".text_decore",{
+    duration:0.2,
+    y:10,
+    x:40,
+    stagger:0.1,
+    
+    delay:0.1,
+    yoyo:true,
+    
+    
+})
+
+gsap.to(".text_minds",{
+    duration:0.2,
+    y:10,
+    x:40,
+    stagger:0.1,
+    delay:0.5,
+    
+    yoyo:true,
+    
+})
+
+gsap.to(".text_web",{
+    duration:0.2,
+    y:10,
+    x:40,
+    stagger:0.1,
+    delay:0.9,
+    yoyo:true,
+  
+});
+
+gsap.to(".text_passionate",{
+    duration:0.2,
+    y:-10,
+    x:10,
+    stagger:0.2,
+    delay:1.1,
+    yoyo:true,
+    
+ 
+});
+
+gsap.to(".text_engaging",{
+    duration:0.2,
+    y:-10,
+    x:10,
+    stagger:0.2,
+    delay:1.3,
+    yoyo:true,
+    
+  
+});
